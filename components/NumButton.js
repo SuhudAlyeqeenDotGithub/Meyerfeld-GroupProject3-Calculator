f2083168550a3493fe3e9d7590229d427e95d1db
RestartGamer <ckorkmaz56@gmail.com>
2026-09-15 15:46:24 +0200

    feat(project): add initial html and css structure

---
+++ components/NumButton.js
@@ -0,0 +1,11 @@
+function NumButton(text) {
+    return `
+        <div class="num-pad__frame">
+            <div class="num-pad__content">
+                <span>
+                    ${text}
+                </span>
+            </div>
+        </div>
+    `;
+}
